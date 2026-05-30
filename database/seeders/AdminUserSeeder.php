f9c776ba14411cbc75c61e9c58c401f3f7d8da26
Yudhistira Ramadhani <yudhistir4rmrdh4ni@gmail.com>
2026-05-30 20:18:21 +0700

    Add admin seeder

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+{
+    \App\Models\User::updateOrCreate(
+        ['email' => 'projek_aplikasi@gmail.com'],
+        [
+            'name' => 'Admin',
+            'password' => bcrypt('1'),
+        ]
+    );
+}
+}
